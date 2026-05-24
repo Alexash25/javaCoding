a92c386429a60539cfe665610ed61fe419813a57
Ash0525 <alextiu0525@gmail.com>
2026-05-24 12:44:39 -0500

    went over math constants in Java

---
+++ mathConstants/Main.java
@@ -0,0 +1,25 @@
+package mathConstants;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result1;
+        double result2;
+        
+        result1 = Math.pow(2,5);
+        result2 = Math.abs(-5);
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        // you can use Math.ceil() to round up
+        // you can use Math.round() to round
+        // you can use Math.floor() to round down
+
+        // Math.max(10, 20) will find the max
+        // Math.min(10, 20) will find the min
+    }
+}
--- randomNumbers/Main.java
+++ randomNumbers/Main.java
@@ -5,11 +5,26 @@ public class Main {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int number;
+        int number1;
+        int number2;
+        int number3;
 
-        number = random.nextInt(1, 6);
+        // first number is inclusive, last number is exclusive
+        number1 = random.nextInt(1,7);
+        number2 = random.nextInt(1,7);
+        number3 = random.nextInt(1,7);
 
-        System.out.println(number);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
 
+        // generate random doubles
+        double numDouble;
+
+        numDouble = random.nextDouble();
+
+        System.out.println(numDouble);
+
+        // you can also randomize bools
     }
 }

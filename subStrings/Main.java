8b613c073c343a37e1b83cbc07e125eb76ae307d
Alexander Tiu <alextiu0525@gmail.com>
2026-05-25 17:29:29 -0500

    I learned about substrings hooray. I need to make an email/password program or something

---
+++ subStrings/Main.java
@@ -0,0 +1,38 @@
+package subStrings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email: ");
+
+        // .substring() = A method used to extract a portion of a string
+        // string.substring(start, end)
+
+        // create an email slicer program
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            // substring is exclusive at the end
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf('@') + 1);
+
+
+
+            System.out.println("A good username for you would be: " + username);
+            System.out.println("Domain of your email: " + domain);
+        } else {
+            System.out.println("Emails must contain '@'");
+        }
+
+
+        scanner.close();
+        
+    }
+}
